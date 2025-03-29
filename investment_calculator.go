f737b7e34ce06f647ba7a2a3a0ab76ff8cdb64e6
Ojo <oluwatobiojo2911@gmail.com>
2025-03-29 19:31:34 +0100

    refactor main function to accept user input for investment calculations

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,15 +1,31 @@
 package main
 
-import( 
+import (
 	"fmt"
 	"math"
 )
 
-func main() {
-	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+// func main() {
+// 	var investmentAmount float64 = 1000
+// 	var expectedReturnRate = 5.5
+// 	var years float64 = 10
 
-	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate /100, years)
-	fmt.Println(futureValue)	
+// 	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate /100, years)
+// 	fmt.Println(futureValue)
+// }
+
+
+func main(){
+	 var investmentAmount, expectedReturnRate, years float64
+	 
+
+	 fmt.Print("Enter Investment Amount: ")
+	 fmt.Scan(&investmentAmount);
+	 fmt.Print("Expected Return Rate: ")
+	 fmt.Scan(&expectedReturnRate)
+	 fmt.Print("Years: ")
+	 fmt.Scan(&years)
+	 futureValue :=  investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
+	fmt.Println(futureValue)
+
 }
