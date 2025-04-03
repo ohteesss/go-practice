1e5402dcd5d3c7f793ac6bbca86eb76b0c4fa50e
Ojo <oluwatobiojo2911@gmail.com>
2025-04-03 21:31:45 +0100

    add inflation rate constant and refactor future value calculation

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,6 +5,8 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 // func main() {
 // 	var investmentAmount float64 = 1000
 // 	var expectedReturnRate = 5.5
@@ -25,7 +27,29 @@ func main(){
 	 fmt.Scan(&expectedReturnRate)
 	 fmt.Print("Years: ")
 	 fmt.Scan(&years)
-	 futureValue :=  investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
+
+	futureValue,formattedFV := calculateFutureValues(investmentAmount,expectedReturnRate, years)
+	// fmt.Printf("Future Value: %.2f\n", futureValue)
+	fmt.Print(formattedFV)
 	fmt.Println(futureValue)
 
 }
+
+
+
+
+// func calculateFutureValues(investmentAmount,expectedReturnRate, years float64) (float64, float64){
+// 	fv := investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
+// 	rfv := fv / math.Pow(1+inflationRate/100, years)
+
+// 	return fv,rfv
+// }
+func calculateFutureValues(investmentAmount,expectedReturnRate, years float64) (fv float64, rfv float64){
+	fv = investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
+	rfv = fv / math.Pow(1+inflationRate/100, years)
+
+
+	return fv, rfv
+	// return
+}
+
